e55699431fd213363faa2c131992875e01ffd130
andreyvz702 <andreyvz702@gmail.com>
2026-01-09 18:43:28 +0300

    Форматы представления данных

---
+++ json_example.py
@@ -0,0 +1,22 @@
+import json
+
+json_data = '[{"name": "Иван", "age": 30, "is_student": false}]'
+parsed_data = json.loads(json_data)
+
+# print(parsed_data, type(parsed_data), type(parsed_data[0]))
+
+
+data = {"name": "Иван",
+        'age': 30,
+        "is_student": False}
+
+json_string = json.dumps(data, indent=4)
+print(json_string)
+
+
+# with open('json_example.json', encoding='utf-8') as file:
+#     data = json.load(file)
+#     print(data, type(data))
+
+# with open('data.json', 'w', encoding='utf-8') as file:
+#     json.dump(data, file, indent=4, ensure_ascii=False)
---
+++ xml_example.py
@@ -0,0 +1,21 @@
+import xml.etree.ElementTree as ET
+
+xml_data = """
+<user>
+    <id>1</id>
+    <first_name>John</first_name>
+    <last_name>Doe</last_name>
+    <email>john.doe@example.com</email>
+    <address>
+        <street>Main Street q</street>
+        <city>New York</city>
+        <zip>10001</zip>
+    </address>
+</user>
+"""
+
+root = ET.fromstring(xml_data)
+
+print('User ID:', root.find('id').text)
+print('User Name:', root.find('first_name').text)
+print('User Email:', root.find('email').text)
